83365316e88d3d97789702c3e737d46595f1d058
Henrique Santana <hsatana.sousass@gmail.com>
2023-11-07 22:54:28 -0300

    relacionamento to_do_list

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable,
          :rememberable, :validatable, :trackable
+
+  has_many :to_do_lists, dependent: :destroy
 end
