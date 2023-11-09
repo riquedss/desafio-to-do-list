da3b493450526b7e7df3112e7652ea815c2e8b54
Henrique Santana <hsatana.sousass@gmail.com>
2023-11-09 20:07:36 -0300

    feat(signup): controller

---
+++ app/controllers/concerns/auth/signup_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Auth
+  class SignupController < ApplicationController
+    def new
+      @user ||= User.new
+
+      render :new
+    end
+
+    def create
+      @user = User.new(params_signup)
+
+      if @user.save
+        @user = User.new
+        render turbo_stream: turbo_stream.replace('turbo-frame', partial: 'success')
+      else
+        render turbo_stream: turbo_stream.replace('turbo-frame', partial: 'erro')
+      end
+    end
+
+    private
+
+    def params_signup
+      params.require(:user).permit(:name, :last_name, :email, :birthday, :password, :password_confirmation)
+    end
+  end
+end
