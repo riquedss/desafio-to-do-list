2b331f2cf1acad07fc44b9af1b74762bda04b134
Henrique Santana <hsatana.sousass@gmail.com>
2023-11-10 01:46:58 -0300

    refactor: retirando obrigatoriedade da tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_10_010654) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_10_034955) do
   create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -41,7 +41,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_010654) do
     t.bigint "to_do_list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "tag_id", null: false
+    t.bigint "tag_id"
+    t.integer "status", default: 0
     t.index ["tag_id"], name: "index_tasks_on_tag_id"
     t.index ["to_do_list_id"], name: "index_tasks_on_to_do_list_id"
   end
