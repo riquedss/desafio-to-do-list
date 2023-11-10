37e01bd89e60050db4be029ef2db2be44e27466d
Henrique Santana <hsatana.sousass@gmail.com>
2023-11-10 13:14:41 -0300

    feat (user): validações

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ApplicationRecord
 
   has_many :to_do_lists, dependent: :destroy
   has_many :tags, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates_length_of :password, minimum: 8, allow_blank: true, message: 'precisa ter no mínimo 8 caracteres'
+  validates_presence_of :role
 end
