3859265e9229cf1e149d93b4b0df92ff51ac32ef
Henrique Santana <hsatana.sousass@gmail.com>
2023-11-10 01:45:19 -0300

    feat: validação e add status

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,9 @@
 
 class Task < ApplicationRecord
   belongs_to :to_do_list
-  belongs_to :tag
+  belongs_to :tag, optional: true
+
+  enum status: %i[para_fazer fazendo finalizada]
+
+  validates :name, presence: true
 end
--- app/models/to_do_list.rb
+++ app/models/to_do_list.rb
@@ -3,4 +3,6 @@
 class ToDoList < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
+
+  validates :title, presence: true
 end
