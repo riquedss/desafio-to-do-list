37c9d7c66cdbef3e1f26fa4a6f66546623012320
Henrique Santana <hsatana.sousass@gmail.com>
2023-11-14 07:56:33 -0300

    refactor: novo fluxo de mostrar erro de cadastro

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,26 @@ module ApplicationHelper
   def date_field_with_label(form, attribute, label_text)
     form.label(label_text) + form.date_field(attribute)
   end
+
+  def mostra_erro(user)
+    erros = format_array_erros(user)
+    tam = erros.count
+
+    part = erros.slice(0, tam - 1)
+    part = part.join(', ')
+    part += ' e ' unless part.blank?
+    part + erros[tam - 1]
+  end
+
+  private
+
+  def format_array_erros(user)
+    erros = []
+
+    user.errors.messages.each do |k, v|
+      erros.push("#{I18n.t("activerecord.user.#{k}")} #{v[0]}")
+    end
+
+    erros
+  end
 end
